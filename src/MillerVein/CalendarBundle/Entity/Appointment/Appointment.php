ae0c8a99d76c427156b98b519fcd306e8b3ee78a
Nick Fenzan <nickf@millervein.com>
2015-04-06 09:37:45 -0400

    Removed unnecessary use statement that was causing doctrine issues

--- src/MillerVein/CalendarBundle/Entity/Appointment/Appointment.php
+++ src/MillerVein/CalendarBundle/Entity/Appointment/Appointment.php
@@ -4,7 +4,6 @@ namespace MillerVein\CalendarBundle\Entity\Appointment;
 
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
-use MillerVein\CalendarBundle\Entity\AppointmentRepository;
 use MillerVein\CalendarBundle\Entity\AppointmentStatus;
 use MillerVein\CalendarBundle\Entity\Category\Category;
 use MillerVein\CalendarBundle\Entity\Column;
