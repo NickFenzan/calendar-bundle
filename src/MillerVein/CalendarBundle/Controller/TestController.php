c8678da3239f9c5926ceb801a784ad81f2c908a9
Nick Fenzan <nickf@millervein.com>
2015-04-14 10:44:27 -0400

    Fixed date on import function

--- src/MillerVein/CalendarBundle/Controller/TestController.php
+++ src/MillerVein/CalendarBundle/Controller/TestController.php
@@ -20,20 +20,6 @@ use Symfony\Component\HttpFoundation\Response;
 class TestController extends Controller {
 
     /**
-     * @Route("/test/1")
-     */
-    public function test1Action(){
-        $em = $this->getDoctrine()->getManager();
-        $columnRepo = $em->getRepository('MillerVeinCalendarBundle:Column');
-//        $cols = $columnRepo->findBy(array('legacy_provider'=>13));
-        $cols = $columnRepo->findBy(array('legacy_provider'=>10),array('id'=>'DESC'));
-        if($cols){
-            echo $cols[0]->getId(). ' - '. $cols[0]->getName();
-        }
-        return new Response();
-    }
-    
-    /**
      * @Route("/importOldAppointments", name="apptImport")
      */
     public function testAction() {
@@ -67,7 +53,7 @@ class TestController extends Controller {
                 $newAppt->setId($oldAppt['pc_eid']);
                 
                 // <editor-fold defaultstate="collapsed" desc="Column">
-                if($oldAppt['pc_eventDate'] >='2014-04-01'){
+                if($oldAppt['pc_eventDate'] >='2015-04-01'){
                     $cols = $columnRepo->findBy(array('legacy_provider'=>$oldAppt['pc_aid']),array('id'=>'DESC'));
                 }else{
                     $cols = $columnRepo->findBy(array('legacy_provider'=>$oldAppt['pc_aid']));
@@ -135,104 +121,4 @@ class TestController extends Controller {
         }
 //        print_r($users);
     }
-
-        /**
-     * @Route("/importNewAppointments", name="apptImportNew")
-     */
-    public function importNewAction() {
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
-
-        $em = $this->getDoctrine()->getManager();
-        $db = $em->getConnection();
-        $columnRepo = $em->getRepository("MillerVeinCalendarBundle:Column");
-        $categoryRepo = $em->getRepository("MillerVeinCalendarBundle:Category\PatientCategory");
-        $apptStatusRepo = $em->getRepository("MillerVeinCalendarBundle:AppointmentStatus");
-        $patientRepo = $em->getRepository("MillerVeinEMRBundle:PatientData");
-
-        $maxAppt = $db->fetchColumn('SELECT MAX(id) FROM calendar.appointment');
-        if(!$maxAppt){
-            $maxAppt = 0;
-        }
-        $workingAppts = $db->fetchAll('SELECT * FROM openemr.openemr_postcalendar_events e '
-                . ' JOIN openemr.openemr_postcalendar_categories c '
-                . ' ON e.pc_catid = c.pc_catid '
-                . ' WHERE pc_eid > ? '
-                . ' AND e.pc_eventDate >= "2015-04-01" '
-                . ' AND c.pc_cattype = 0 '
-                . ' ORDER BY pc_eid ASC LIMIT 1000', array($maxAppt));
-
-        if ($workingAppts) {
-
-            foreach ($workingAppts as $oldAppt) {
-                $newAppt = new PatientAppointment();
-
-                $newAppt->setId($oldAppt['pc_eid']);
-                
-                // <editor-fold defaultstate="collapsed" desc="Column">
-                $cols = $columnRepo->findBy(array('legacy_provider'=>$oldAppt['pc_aid']),array('id'=>'DESC'));
-                if ($cols) {
-                    $column = $cols[0];
-                }else{
-                    $column = $columnRepo->find(4);
-                }
-                $newAppt->setColumn($column);
-                // </editor-fold>
-                // <editor-fold defaultstate="collapsed" desc="Category">
-                $cats = $categoryRepo->findBy(array('legacy_id'=>$oldAppt['pc_catid']));
-                if ($cats) {
-                    $category = $cats[0];
-                }else{
-                    $category = $categoryRepo->find(20);
-                }
-                $newAppt->setCategory($category);
-                // </editor-fold>
-                // <editor-fold defaultstate="collapsed" desc="Status">
-                $apptStatus = $apptStatusRepo->findOneBy(array('legacy_id' => $oldAppt['pc_apptstatus']));
-                if (!$apptStatus) {
-                    $apptStatus = $apptStatusRepo->findOneBy(array('legacy_id' => '-'));
-                }
-                $newAppt->setStatus($apptStatus);
-                // </editor-fold>
-                // <editor-fold defaultstate="collapsed" desc="DateTime">
-                $newAppt->setStart(new DateTime($oldAppt['pc_eventDate'] . ' ' . $oldAppt['pc_startTime']));
-                // </editor-fold>
-                // <editor-fold defaultstate="collapsed" desc="Duration">
-                try{
-                    $startTime = new DateTime($oldAppt['pc_startTime']);
-                    $endTime = new DateTime($oldAppt['pc_endTime']);
-                    $difference = $startTime->diff($endTime);
-                    $minutes = $difference->h * 60;
-                    $minutes += $difference->i;
-                } catch (Exception $ex) {
-                    $minutes = 15;
-                }
-
-                $newAppt->setDuration($minutes);
-                // </editor-fold>
-                // <editor-fold defaultstate="collapsed" desc="Notes">
-                $newAppt->setNotes($oldAppt['pc_hometext']);
-                // </editor-fold>
-
-                $patient = $patientRepo->find($oldAppt['pc_pid']);
-                if ($patient) {
-                    $newAppt->setPatient($patient);
-                }
-
-                $em->persist($newAppt);
-                
-                $metadata = $em->getClassMetaData(get_class($newAppt));
-                $metadata->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
-
-            }
-
-            $em->flush();
-//            return new Response('Success');
-            return $this->redirectToRoute('apptImportNew');
-        } else {
-            return new Response('Success');
-        }
-//        print_r($users);
-    }
-    
 }
